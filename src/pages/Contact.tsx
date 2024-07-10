27345ebf18740972d93cf33d57679e0a605476cc
Aman joshi <105578894+amanjoshi2002@users.noreply.github.com>
2024-07-10 13:16:17 +0000

    changed logo

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,9 +7,6 @@ function Contact() {
     <iframe
       width="100%"
       height="100%"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
       title="map"
       scrolling="no"
       src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
